0c5a8d332a86a4214cfc9a17e46e3208dbec564f
WantedHorizon <eliran.amzalag@gmail.com>
2020-11-08 14:20:59 +0200

    no user img

--- src/components/form/Comment.js
+++ src/components/form/Comment.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Comment as CommentSemanticUI } from 'semantic-ui-react'
 
-const Comment = ({ autour = "TEST", msg = "no message provided", imgSrc }) => {
+const Comment = ({ autour = "TEST", msg = "no message provided", imgSrc="https://www.pikpng.com/pngl/m/16-168770_user-iconset-no-profile-picture-icon-circle-clipart.png" }) => {
     return (
         <CommentSemanticUI>
             <CommentSemanticUI.Avatar src={imgSrc} />
